2c0b862e10f53f0c5648860ccfab2770cf79ed74
Olha Melska <olya.melska@gmail.com>
2018-08-08 16:20:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 /* Enter the code to remove the main node element under this comment */
 
+main.remove()
+
+/*document.getElementById('main').remove()*/
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+
+const newHeader = document.createElement('h1')
+document.body.appendChild(newHeader)
+newHeader.setAttribute("id", "victory")
+newHeader.innerText = 'Olya is the champion!'
