c388143403e5060b597d5a4534b12480f36d35ac
Jason Carter <jasonbradleycarter@gmail.com>
2018-08-21 21:49:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+
 /* Enter the code to remove the main node element under this comment */
 
+document.querySelector("main#main").remove();
+
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.innerText = "Jason is the champion";
+
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+
+
